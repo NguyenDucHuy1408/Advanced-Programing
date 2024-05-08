7430452cca456575d9f6739533246b2db4a95377
NguyenDucHuy1408 <nguyenduchuy267148@icloud.com>
2024-05-08 13:34:46 +0700

    Update sound and music

--- BTL/sound.cpp
+++ BTL/sound.cpp
@@ -2,7 +2,8 @@
 
 Sound::Sound()
 {
-
+    music = NULL;
+    chunk = NULL;
 }
 
 Sound::~Sound()
@@ -12,5 +13,35 @@ Sound::~Sound()
 
 Mix_Music* Sound::loadMusic(const char* path)
 {
+    music = Mix_LoadMUS(path);
+    if(music == NULL)
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Could not load music! SDL_mixer Error: %s", Mix_GetError());
 
+    return music;
+}
+
+void Sound::playMusic()
+{
+    if(music == NULL) return;
+
+    if(Mix_PlayingMusic() == 0)
+        Mix_PlayMusic(music, -1);
+
+    else if(Mix_PausedMusic() == 1)
+        Mix_ResumeMusic();
+}
+
+Mix_Chunk* Sound::loadChunk(const char* path)
+{
+    chunk = Mix_LoadWAV(path);
+    if(chunk == NULL)
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Could not load sound! SDL_mixer Error: %s", Mix_GetError());
+
+    return chunk;
+}
+
+void Sound::playChunk()
+{
+    if(chunk != NULL)
+        Mix_PlayChannel(-1, chunk, 0);
 }
--- BTL/sound.h
+++ BTL/sound.h
@@ -5,12 +5,19 @@ class Sound
 {
 public:
     Sound();
+
     ~Sound();
+
     Mix_Music* loadMusic(const char* path);
+
     void playMusic();
+
     Mix_Chunk* loadChunk(const char* path);
+
     void playChunk();
+
 private:
     Mix_Music *music;
+
     Mix_Chunk *chunk;
 };
